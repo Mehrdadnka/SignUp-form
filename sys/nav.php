32bac59a9d756426ae3d8fb64e09e2bd52d9491c
Mehrdad <100048050+Mehrdadnka@users.noreply.github.com>
2023-01-04 23:35:18 +0330

    Update nav.php

--- sys/nav.php
+++ sys/nav.php
@@ -3,9 +3,9 @@
     <li>Brand</li>
     <li class="float-right sticky"></li>
     <li><a href="index.php">Home </a></li>
-    <li><a href="#">Services</a>
+    <li><a href="#">New user?</a>
       <ul>
-        <li><a href="../includes/signup.php">New user?</a></li>
+        <li><a href="../includes/signup.php">Sign Up</a></li>
       </ul>
     </li>
   </ul>
